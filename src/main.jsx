9ddb4834a255c1e07ac60556a2d6c6711db31502
zhonghuangx-prog <zhonghuangx@gmail.com>
2026-06-05 01:44:10 +0800

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const Router =
+  window.location.hostname.endsWith('github.io') ? HashRouter : BrowserRouter;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+);
